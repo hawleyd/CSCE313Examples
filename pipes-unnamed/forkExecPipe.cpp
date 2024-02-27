4bdef89edd92a4564661ecb307d256893d74af0f
Doug Hawley <doug@tamu.edu>
2024-02-27 18:14:26 +0000

    finished forkExecPipe

--- pipes-unnamed/forkExecPipe.cpp
+++ pipes-unnamed/forkExecPipe.cpp
@@ -3,9 +3,21 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main() {
     
+    int status;
+
+    int pipefds[2];
+    char buf[30];
+
+    if (pipe(pipefds) ==-1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    memset(buf,0,30);
 
     pid_t child_pid = fork();
 
@@ -17,14 +29,25 @@ int main() {
     if (child_pid == 0) { // Child process
         std::cout << "Hello from the child process!\nThe parent process ID is " << getppid() << std::endl;
 
+        std::cout << "Child is writing in the pipe" << std::endl;
+        close (pipefds[0]);     //close read end
+        write(pipefds[1], "I'm your favorite child", 24);
+        close(pipefds[0]);      //close write end
+
         sleep(1); // Sleep for 6 seconds
         execl("/bin/ls", "ls", "-l", nullptr); // Execute ls -l
         exit(0); // Exit normally
     } 
     else { // Parent process
-        int status;
+
         wait(&status);
+
+        close(pipefds[1]);  //close write end
+        read(pipefds[0], buf, 24);
+        close(pipefds[0]);  //close read end
+
         std::cout << "Hello from the parent process!\n";
+        std::cout << "Parent received: " << buf << std::endl;
         std::cout << "Child  process ID was " << child_pid << std::endl;        
         
         if (WIFEXITED(status)) {
